20bbf35c66d649a99bbc495b557f3a3c80a60635
benjaminhlina <benjamin.hlina@gmail.com>
2017-10-17 23:23:54 -0400

    Updated Documentation

--- R/lampreytox.R
+++ R/lampreytox.R
@@ -12,7 +12,7 @@
 #'   \item{survive}{number of lamprey that survived over 12 h exposure to TFM}
 #'   \item{total}{total number of lamprey that were exposed to TFM over 12 h}
 #'   }
-#' @details For each month, new lamprey were collected from the deer creek, MI, and were randomly assigned to a tank for exposure to varying doses of the piscicide, 3-trifluoromethly-4-nitrophenol (TFM).
+#' @details For each month, new lamprey were collected from the deer creek, MI, and were randomly assigned to a tank for exposure to varying doses of the piscicide, 3-trifluoromethyl-4-nitrophenol (TFM).
 #' @source Hlina, B.L., Birceanu, O., Robinson, C., Tessier, L.R., Muhametsafina, A., Bragg, L.M, Servos, M.R., Wilkie, M.P. In Preparation. Changes in the sensitivity of piscicide in an invasive species. Environmental Science & Technology.
 
 "lampreytox"
