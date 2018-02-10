5e902b1d454a885e6109c1d730866cc83629cef0
benjaminhlina <benjamin.hlina@gmail.com>
2018-02-09 23:33:10 -0500

    Updated test

--- tests/testthat/test-LC_probit.R
+++ tests/testthat/test-LC_probit.R
@@ -3,7 +3,7 @@ context("test-LCprobit.R")
 test_that("Determine if LCprobit calculations are correct", {
 
 LC_probit((dead / total) ~ log10(dose), p = c(50, 99),
-                 weights = lampreytox[c(1:19), ]$total,
-                data = lampreytox[c(1:19), ])
+               weights = total,
+               data = lampreytox,
+               subset = c(month == "May"))
 })
-
