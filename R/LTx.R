8aa2fd407b8fa14b580d7057a1ca6a3d8229a359
benjaminhlina <benjamin.hlina@gmail.com>
2019-03-20 16:53:11 -0400

    Updated help files to state that LT_probit and LT_logit can now output values for LTs down to 0.1

--- R/LTx.R
+++ R/LTx.R
@@ -1,16 +1,15 @@
-# Descirption of LT_probit ----
+# Description of LT_probit ----
 
 #' Lethal Time Probit
 #' @description Calculates lethal time (LT) and
 #' its fiducial confidence limits (CL) using a probit analysis
 #' according to Finney 1971, Wheeler et al. 2006, and Robertson et al. 2007.
-#' @usage LT_probit(formula, data, p = seq(1, 99, 1), weights,
+#' @usage LT_probit(formula, data, p = seq(0.1, 99.9, 0.1), weights,
 #'           subset = NULL, log_x = TRUE, het_sig = NULL, conf_level = NULL,
 #'           long_output = TRUE)
 #' @param formula an object of class `formula` or one that can be coerced to that class: a symbolic description of the model to be fitted.
 #' @param data an optional data frame, list or environment (or object coercible by as.data.frame to a data frame) containing the variables in the model. If not found in data, the variables are taken from environment(formula), typically the environment from which `LT_probit` is called.
-#' @param p Lethal time (LT) values for given p, example will return a LT50 value if p equals 50. If more than one LT value desired specify by creating a vector.
-#'
+#' @param p Lethal time (LT) values for given p, example will return a LT50 value if p equals 50. If more than one LT value desired specify by creating a vector. LT values can be calculated down to the 0.1 of a percentage (e.g. LT75.5)
 #' @param weights vector of 'prior weights' to be used in the fitting process. Should be a numeric vector and is required for analysis.
 #' @param subset allows for the data to be subseted if desired. Default set to `NULL`.
 #' @param log_x Default is `TRUE` and will calculate results using the antilog10 given that the x variable has been `log10` tranformed. If `FALSE` results will not be back transformed.
@@ -239,18 +238,18 @@ LT_probit <- function(formula, data, p = seq(1, 99, 1),
 
 }
 
-# Descirption of LT_logit ----
+# Description of LT_logit ----
 
 #' Lethal Time Logit
 #' @description Calculates lethal time (LT) and
 #' its fiducial confidence limits (CL) using a logit analysis
 #' according to Finney 1971, Wheeler et al. 2006, and Robertson et al. 2007.
-#' @usage LT_logit(formula, data, p = seq(1, 99, 1), weights,
+#' @usage LT_logit(formula, data, p = seq(0.1, 99.9, 0.1), weights,
 #'           subset = NULL, log_x = TRUE, het_sig = NULL,
 #'           conf_level = NULL, long_output = TRUE)
 #' @param formula an object of class `formula` or one that can be coerced to that class: a symbolic description of the model to be fitted.
 #' @param data an optional data frame, list or environment (or object coercible by as.data.frame to a data frame) containing the variables in the model. If not found in data, the variables are taken from environment(formula), typically the environment from which `LT_logit` is called.
-#' @param p Lethal time (LT) values for given p, example will return a LT50 value if p equals 50. If more than one LT value desired specify by creating a vector.
+#' @param p Lethal time (LT) values for given p, example will return a LT50 value if p equals 50. If more than one LT value desired specify by creating a vector. LT values can be calculated down to the 0.1 of a percentage (e.g. LT75.5)
 #' @param weights vector of 'prior weights' to be used in the fitting process. Should be a numeric vector and is required for analysis.
 #' @param subset allows for the data to be subseted if desired. Default set to `NULL`.
 #' @param log_x Default is `TRUE` and will calculate results using the antilog10 given that the x variable has been `log10` tranformed. If `FALSE` results will not be back transformed.
