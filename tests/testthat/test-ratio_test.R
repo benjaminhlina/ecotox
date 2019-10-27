b4e4f1dd71767ecf5c68786407b75ec5b53f68fa
Benjamin Hlina <benjamin.hlina@gmail.com>
2019-10-27 12:12:31 -0400

    Added tests to test errors for ratio_test

--- tests/testthat/test-ratio_test.R
+++ tests/testthat/test-ratio_test.R
@@ -1,5 +1,7 @@
 context("Test ratio_test")
 
+
+# test if ratio_test works properly -----
 test_that("Determine if ratio_test runs properly", {
 
 s <- glm((response / total) ~ log10(dose),
@@ -25,11 +27,46 @@ expect_equal(ratios$dose_1, expected = 2.66, tolerance = 0.001)
   expect_equal(ratios$test_stat, expected = 2.544, tolerance = 0.001)
   expect_equal(ratios$p_value, expected =  0.0109, tolerance = 0.0001)
 
+})
 
 
-# ratios_m1 <- ratio_test( model_2 = s, percentage = 50)
-#
-# expect_
+# test errors -----
+
+test_that("Test errors if model 2 isn't supplied", {
+  s <- glm((response / total) ~ log10(dose),
+           data = lamprey_tox[lamprey_tox$nominal_dose != 0, ],
+           subset = c(month == "September"),
+           weights = total,
+           family = binomial(link = "probit"))
+
+
+  j <- glm((response / total) ~ log10(dose),
+           data = lamprey_tox[lamprey_tox$nominal_dose != 0, ],
+           subset = c(month == "June"),
+           weights = total,
+           family = binomial(link = "probit"))
+
+  expect_error(ratio_test(model_1 = j, percentage = 50))
+
+
+
+})
+
+test_that("Test errors if model 1 isn't supplied", {
+  s <- glm((response / total) ~ log10(dose),
+           data = lamprey_tox[lamprey_tox$nominal_dose != 0, ],
+           subset = c(month == "September"),
+           weights = total,
+           family = binomial(link = "probit"))
+
+
+  j <- glm((response / total) ~ log10(dose),
+           data = lamprey_tox[lamprey_tox$nominal_dose != 0, ],
+           subset = c(month == "June"),
+           weights = total,
+           family = binomial(link = "probit"))
+
+expect_error(ratio_test(model_2 = s, percentage = 50))
 
 
 
