1d391f82a9c3d9085424b8d5e32aedf84c7be495
Benjamin Hlina <benjamin.hlina@gmail.com>
2020-03-05 15:11:33 -0500

    Forcing model to be weighted causes issues with cbind() method for response variable. Error message as been fixed and now if you don't supply weights you will get a warning to double check if you are using the right method.

--- R/LCx.R
+++ R/LCx.R
@@ -113,8 +113,8 @@ LC_probit <- function(formula, data, p = NULL,
                                subset = substitute(subset)))
 
   # error message for missing weights argument in function call
-  if(missing(weights)) {
-    stop("Model needs the total of test organsim per dose to weight the model properly",
+  if(is.null(weights)) {
+    warning ("Are you using cbind(response, non-response) method as your y variable, if so do not weight the model. If you are using (response / total) method, model needs the total of test organisms per dose to weight the model properly",
          call. = FALSE)
   }
 
@@ -427,10 +427,11 @@ LC_logit <- function(formula, data, p = NULL, weights = NULL,
                                subset = substitute(subset)))
 
   # error message for missing weights argument in function call
-  if(missing(weights)) {
-    stop("Model needs the total of test organsim per dose to weight the model properly",
-         call. = FALSE)
+  if(is.null(weights)) {
+    warning ("Are you using cbind(response, non-response) method as your y variable, if so do not weight the model. If you are using (response / total) method, model needs the total of test organisms per dose to weight the model properly",
+             call. = FALSE)
   }
+
 
 
   # make p a null object and create warning message if p isn't supplied
--- R/LTx.R
+++ R/LTx.R
@@ -58,10 +58,11 @@ LT_probit <- function(formula, data, p = NULL,
                                subset = substitute(subset)))
 
   # error message for missing weights argument in function call
-  if(missing(weights)) {
-    stop("Model needs the total of test organsim per dose to weight the model properly",
-         call. = FALSE)
+  if(is.null(weights)) {
+    warning ("Are you using cbind(response, non-response) method as your y variable, if so do not weight the model. If you are using (response / total) method, model needs the total of test organisms per dose to weight the model properly",
+             call. = FALSE)
   }
+
 
 
   # make p a null object and create warning message if p isn't supplied
@@ -313,9 +314,9 @@ LT_logit <- function(formula, data, p = NULL, weights = NULL,
                                subset = substitute(subset)))
 
   # error message for missing weights argument in function call
-  if(missing(weights)) {
-    stop("Model needs the total of test organsim per dose to weight the model properly",
-         call. = FALSE)
+  if(is.null(weights)) {
+    warning ("Are you using cbind(response, non-response) method as your y variable, if so do not weight the model. If you are using (response / total) method, model needs the total of test organisms per dose to weight the model properly",
+             call. = FALSE)
   }
 
 
